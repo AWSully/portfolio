42feb20ae5172836f80be22faa2887a0493fe751
Austin Sullivan <AWSully@gmail.com>
2024-05-30 11:20:29 -0400

    working on email functionality

--- Austin/forms/contact.php
+++ Austin/forms/contact.php
@@ -7,7 +7,8 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+ 
+  $receiving_email_address = 'awsully@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +25,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'awsully@gmail.com',
+    'password' => '10020205',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
